d6f06f066b48f3fae5dd8e974ae43f8d67ee92ec
cstella <cestella@gmail.com>
2014-09-03 13:33:01 -0500

    Updating textProcessing

---
+++ nlp/textProcessing/src/main/java/com/hortonworks/example/mr/WholeFileInputFormat.java
@@ -0,0 +1,26 @@
+package com.hortonworks.example.mr;
+
+import java.io.IOException;
+
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+
+public class WholeFileInputFormat extends FileInputFormat<Text, BytesWritable> {
+
+    @Override
+    public RecordReader<Text, BytesWritable> createRecordReader(InputSplit inputSplit, TaskAttemptContext taskAttemptContext) throws IOException, InterruptedException {
+        RecordReader<Text, BytesWritable> ret = new WholeFileRecordReader();
+        ret.initialize((FileSplit)inputSplit, taskAttemptContext);
+        return ret;
+    }
+}
